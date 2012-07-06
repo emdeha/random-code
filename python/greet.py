494949f25e50a299dd0289e912d36e1dd73698b8
Tsvetan Tsvetanov <gogo_rus@abv.bg>
2012-07-06 09:19:01 +0300

    Made little modifications to the source

--- python/greet.py
+++ python/greet.py
@@ -1,7 +1,10 @@
-import fbconsole
+from urllib import urlretrieve
 import imp
+import fbconsole
 import time
 from datetime import date, timedelta
+
+urlretrieve('https://raw.github.com/facebook/fbconsole/master/src/fbconsole.py', 'fbconsole.py')
 fb = imp.load_source('fb', 'fbconsole.py')
 fb.AUTH_SCOPE = ['publish_stream', 'publish_checkins', 'friends_birthday']
 fb.authenticate()
@@ -15,8 +18,12 @@ query = ("SELECT username FROM user WHERE uid IN "
 	     "substr(birthday_date, 3, 5) <= \'" + nextDay.strftime("%d") + "\')")
 names = fb.fql(query)
 
-message = "Chestit rojden den! :)"
-for name in names:
-    path = "/" + name.get('username') + "/feed"
-    fb.graph_post(path, {'message':message})
-
+print "Birthdays greeted: "
+if names == []:
+    print "None"
+else:
+    message = "Chestit rojden den! :)"
+    for name in names:
+        path = "/" + name.get('username') + "/feed"
+        fb.graph_post(path, {'message':message})
+        print name.get('username')
